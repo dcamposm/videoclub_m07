a34b9c45381dd2766ae10e21722f5a476ea57444
Albert <albertmtzmtz@gmail.com>
2019-02-13 16:24:34 +0100

    Albert - Models

---
+++ database/seeds/ActorsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ActorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('actors')->insert([
+            'name' => 'Nicolas Cage',
+            'lastname' => 'Cage',
+            'bday' => \Carbon\Carbon::create(1989, 22, 5),
+            'nationality' => 'USA',
+        ]);
+    }
+}
