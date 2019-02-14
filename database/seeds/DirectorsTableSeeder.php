915ad2e00c6650703f28567ab6ea467482d2ac21
relleno <grupo.relleno@gmail.com>
2019-02-14 20:15:31 +0100

    Demetrio - Seed  genres y Directors

--- database/seeds/DirectorsTableSeeder.php
+++ database/seeds/DirectorsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Director;
 
 class DirectorsTableSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DirectorsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('directors')->delete();
+        
+        Director:: create(array(    
+            'name' => 'Steven',
+            'lastname' => 'Spielberg',
+            'bday' => \Carbon\Carbon::create(1946, 18, 10),
+            'nacionality' => 1
+        ));
+        
+        Director:: create(array(    
+            'name' => 'Demetrio',
+            'lastname' => 'Campos',
+            'bday' => \Carbon\Carbon::create(1997, 21, 9),
+            'nacionality' => 2
+        ));
     }
 }
