718f027b6166df65bff818fd0cb9736c9d8e1b80
Denis <m8.denis.pereira@gmail.com>
2019-02-18 19:23:11 +0100

    Seeds (Comments y Rents)

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -12,6 +12,70 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('comments')->delete();
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '1',
+            'id_client' => '3',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 12),
+            'rate' => '5',
+            'comment' => 'Falso por que las guacamayas no comen queso.'
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '2',
+            'id_client' => '1',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 13),
+            'rate' => '2',
+            'comment' => 'Discrepo pues el apareamiento es totalmente irrelevante en la física nuclear.'
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '3',
+            'id_client' => '3',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 14),
+            'rate' => '3',
+            'comment' => 'Pero lo que no sabian es que los físicos termo-nucleares estan haciendo de las suyas y quieren implementar tecnología broccolil para que los gansitos marinela puedan ser utilizados como guerrereos transatlanticos!! '
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '4',
+            'id_client' => '1',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 15),
+            'rate' => '4',
+            'comment' => 'Pero ten cuidado que las gomitas, están que quieren armar una revolución y alzarse en armas contra el rey bonbon y la princesa masapan por encerrarlos en bolsitas de selofán.'
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '5',
+            'id_client' => '2',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 10),
+            'rate' => '5',
+            'comment' => 'Me gusta el arroz.'
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '6',
+            'id_client' => '3',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 11),
+            'rate' => '4',
+            'comment' => 'Ci sono molti eventi durante la settimana, anche se di Domenica per esempio era vuoto. Quando sono stata, con la musica che cera io due balletti sono fatti; in serenità ed allegria.'
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '7',
+            'id_client' => '2',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 10),
+            'rate' => '3',
+            'comment' => 'Un locale molto caotico ed è difficile trovare un tavolo soprattutto di sabato sera. Cè lobbligo di una consumazione per poter partecipare al karaoke. I prezzi delle consumazioni oscillano tra i 7 e i 9 €. Possibilità di passare una serata diversa con gli amici.'
+        ));
+        
+        Movie_Genre:: create(array(    
+            'id_movies' => '8',
+            'id_client' => '1',
+            'dateComment' => \Carbon\Carbon::create(2019, 1, 14),
+            'rate' => '2',
+            'comment' => 'We cannot simply sit back and allow these matters to develop in a random way.'
+        ));
     }
 }
--- database/seeds/RentsTableSeeder.php
+++ database/seeds/RentsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Rents;
 
 class RentsTableSeeder extends Seeder
 {
@@ -15,66 +16,66 @@ class RentsTableSeeder extends Seeder
         
         Movie_Genre:: create(array(    
             'id_movies' => '1',
-            'id_client' => '1',
+            'id_client' => '3',
             'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 10),
             'price' => '10'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '2',
             'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
-            'price' => '10'
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 2),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 11),
+            'price' => '15'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '3',
-            'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
+            'id_client' => '3',
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 3),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 12),
             'price' => '10'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '4',
             'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
-            'price' => '10'
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 4),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 13),
+            'price' => '12'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '5',
-            'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
+            'id_client' => '2',
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 5),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 14),
             'price' => '10'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '6',
-            'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
+            'id_client' => '3',
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 6),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 15),
             'price' => '10'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '7',
-            'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
-            'price' => '10'
+            'id_client' => '2',
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 7),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 16),
+            'price' => '12'
         ));
         
         Movie_Genre:: create(array(    
             'id_movies' => '8',
             'id_client' => '1',
-            'dateRent' => \Carbon\Carbon::create(2019, 1, 1),
-            'dateReturnRent' => \Carbon\Carbon::create(1991, 13, 2),
-            'price' => '10'
+            'dateRent' => \Carbon\Carbon::create(2019, 1, 8),
+            'dateReturnRent' => \Carbon\Carbon::create(2019, 1, 17),
+            'price' => '15'
         ));
     }
     /*
