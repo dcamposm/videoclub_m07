d8f5fa572c19fa4521899340ecd834cc3bd3cfc7
Albert <albertmtzmtz@gmail.com>
2019-02-18 19:34:25 +0100

    Albert - Model Country

--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    protected $table = 'countries';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'flag'
+    ];
+
+    public function movies()
+    {
+        return $this->hasMany('App\Movie', 'id', 'country');
+    }
+
+    public function directors()
+    {
+        return $this->hasMany('App\Director', 'id', 'nationality');
+    }
+
+    public function actors()
+    {
+        return $this->hasMany('App\Actor', 'id', 'nationality');
+    }
+
+    public function clients()
+    {
+        return $this->hasMany('App\Client', 'id', 'nationality');
+    }
 }
